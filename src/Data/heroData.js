968761a8cce87cfd803dec9796cc0459b54941a1
Yugam Patel <yugampatel@gmail.com>
2024-06-11 15:26:24 -0500

    Update heroData.js

--- src/Data/heroData.js
+++ src/Data/heroData.js
@@ -1,6 +1,6 @@
 export const heroData = {
   heroImage: {
-    isRandom: true,
+    isRandom: false,
     randomImg:
       "https://source.unsplash.com/random/?&mountains&forest&gradient&galaxy&ocean&landscape",
     img: "https://images.unsplash.com/photo-1716668596098-40ff5f73fb9e?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
