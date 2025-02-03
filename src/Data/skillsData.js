368ce9143f7b21cdb2ea111d34fb57e0ac7094f8
Yugam Patel <yugampatel@gmail.com>
2025-02-02 23:55:41 -0600

    Minor bug fix

--- src/Data/skillsData.js
+++ src/Data/skillsData.js
@@ -1,5 +1,5 @@
 export const SkillsData = {
-  skillsOne: ["JavaScript", "React.js", "Vite.js", "Node.js", "Github", "Git", "Thunder-Client", "HTML", "CSS", "SCSS", "MongoDB", "Netlify", "Java", "Mongoose", ],
+  skillsOne: ["JavaScript", "React.js", "Vite.js", "Node.js", "Github", "Git", "Thunder-Client", "HTML", "CSS", "SCSS", "MongoDB", "Netlify", "Java", "Mongoose"],
 
   skillsTwo: ["AWS", "Unix", "SQL", "Express", "REST APIs", "GSAP", "Framer Motion", "Flutter","Responsive Design", "Bootstrap", "Tailwind CSS", "Redux", "Redux Toolkit"],
 
