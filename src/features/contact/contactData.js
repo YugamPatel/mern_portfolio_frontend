385be76a4b238514a7a123af94d68468410ebbe4
Yugam Patel <yugampatel@gmail.com>
2026-05-15 18:31:34 -0500

    Update contactData.js

--- src/features/contact/contactData.js
+++ src/features/contact/contactData.js
@@ -30,7 +30,7 @@ export const contactData = {
   ],
   button: {
     text: "Download Resume",
-    url: "https://drive.google.com/file/d/1zYO_QN3ajWdsdCQOQKv6bl1eBOxfbXGE/view?usp=sharing",
+    url: "https://drive.google.com/file/d/1zYO_QN3ajWdsdCQOQKv6bl1eBOxfbXGE/view",
   },
   sendButton: {
     text: "Say Hi! 👋",
